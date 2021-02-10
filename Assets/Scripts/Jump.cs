28f21894407989690cccd64b53c15df31dbbf86b
Satournine <32436768+Satournine@users.noreply.github.com>
2021-02-10 10:26:18 +0300

    commit

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private bool grounded = true;
+    Rigidbody ball;
+    Vector3 jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GetComponent<Rigidbody>();
+        jumpForce = new Vector3(0, 200f, 0);
+    }
+
+    // Update is called once per frame
+
+    public void ballJumper() //Adds upwards force to ball.
+    {
+        if (grounded)
+        {
+            ball.AddForce(jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision) // To prevent double jump, check if ball is on the ground.
+    {
+        grounded = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        grounded = false;
+    }
+}
---
+++ Assets/Scripts/PressButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressButton : MonoBehaviour
+{
+    public UnityEvent buttonClick;
+
+    private void Awake()
+    {
+        if(buttonClick == null)
+        {
+            buttonClick = new UnityEvent();
+        }
+    }
+
+    void OnMouseUp() //calls buttons functions on click
+    {
+        buttonClick.Invoke();
+    }
+
+}
